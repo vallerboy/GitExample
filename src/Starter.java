f0a1dac2be9a5f09c5629eac743d40c442c5970f
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-11-12 10:12:55 +0100

    add print method in main

--- src/Starter.java
+++ src/Starter.java
@@ -2,4 +2,8 @@ public class Starter {
     public static void main(String[] args) {
         System.out.println("Welcome to the git");
     }
+
+    public static void printSomeText(String text){
+        System.out.println(text);
+    }
 }
